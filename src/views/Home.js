7bf8712e3085ebf3b786ab4dcddd44c1e8c3e81e
luke0han <lukeohanlon@outlook.com>
2023-07-15 16:33:22 +0100

    fetch data

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase';
 
 const Home = () => {
+  const [info, setInfo] = useState([]);
+
+  useEffect(() => {
+    async function getInfo() {
+      try {
+        const nameCollectionRef = collection(db, 'Home-Info');
+        const querySnapshot = await getDocs(nameCollectionRef);
+        const data = querySnapshot.docs.map((doc) => ({
+          data: doc.data(),
+          id: doc.id,
+        }));
+        setInfo(data);
+      } catch (error) {
+        console.log('Error getting info:', error);
+      }
+    }
+    getInfo();
+  }, []);
+
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <h1>Home</h1>
+      {info.map((name, index) => (
+        <div className="point" key={index}>
+          <div className="intro">
+            <h2 className="intro-head">{name.data.Header}</h2>
+            <p className="intro-content">{name.data.Body}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Home;
