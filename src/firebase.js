7d7fa0cf801c91e961df9cb690568d91afb937bd
luke0han <lukeohanlon@outlook.com>
2023-07-15 16:11:37 +0100

    init commit

--- src/firebase.js
+++ src/firebase.js
@@ -12,30 +12,20 @@ import { getStorage, ref } from "firebase/storage";
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-
   apiKey: "AIzaSyB12Ri1wV34IkIH_S6-8DMru3sLbDXEBhk",
-
   authDomain: "galacticgetaways-8c0b1.firebaseapp.com",
-
   projectId: "galacticgetaways-8c0b1",
-
   storageBucket: "galacticgetaways-8c0b1.appspot.com",
-
   messagingSenderId: "129814592021",
-
   appId: "1:129814592021:web:42681b973bfa62793eec1c"
-
 };
 
 
 // Initialize Firebase
-
 const app = initializeApp(firebaseConfig);
-
 const storage = getStorage(app);
 
 // Create a storage reference from our storage service
-
 const storageRef = ref(storage);
 
 export const db = getFirestore(app);
