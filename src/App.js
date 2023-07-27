d200598dadf50548e51dec6db312c01b82a71360
Ken Sheridan <kensheridan86@gmail.com>
2023-07-27 16:01:24 +0100

    merge successful

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ const App = () => {
               <Route path="/privacy" element={<Privacy />} />
             </Routes>
           <Footer />
-        </ScrollToTop>
+        {/* </ScrollToTop> */}
       </div>
   );
 };
